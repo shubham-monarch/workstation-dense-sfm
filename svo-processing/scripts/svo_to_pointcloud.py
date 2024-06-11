4e9ad771bdbb4ed209c9eaaf47be7ac9a73ed673
Shubham Kumar <skumar@monarchtractor.com>
2024-06-12 02:27:22 +0530

    updated svo_to_pointcloud.py

--- svo-processing/scripts/svo_to_pointcloud.py
+++ svo-processing/scripts/svo_to_pointcloud.py
@@ -44,33 +44,6 @@ def parse_camera_parameters(zed):
     }
     return settings_dict
 
-def get_pose(zed,zed_pose, zed_sensors):
-
-    # Get the pose of the left eye of the camera with reference to the world frame
-    zed.get_position(zed_pose, sl.REFERENCE_FRAME.WORLD)
-    zed.get_sensors_data(zed_sensors, sl.TIME_REFERENCE.IMAGE)
-    #zed_imu = zed_sensors.get_imu_data()
-
-    # Display the translation and timestamp
-    py_translation = sl.Translation()
-    
-    tx = round(zed_pose.get_translation(py_translation).get()[0], 3)
-    ty = round(zed_pose.get_translation(py_translation).get()[1], 3)
-    tz = round(zed_pose.get_translation(py_translation).get()[2], 3)
-    print("Translation: Tx: {0}, Ty: {1}, Tz {2}, Timestamp: {3}\n".format(tx, ty, tz, zed_pose.timestamp.get_milliseconds()))
-
-    # Display the orientation quaternion
-    py_orientation = sl.Orientation()
-    ox = round(zed_pose.get_orientation(py_orientation).get()[0], 3)
-    oy = round(zed_pose.get_orientation(py_orientation).get()[1], 3)
-    oz = round(zed_pose.get_orientation(py_orientation).get()[2], 3)
-    ow = round(zed_pose.get_orientation(py_orientation).get()[3], 3)
-    print("Orientation: Ox: {0}, Oy: {1}, Oz {2}, Ow: {3}\n".format(ox, oy, oz, ow))
-
-    pose_dict={'Translation':{'Tx':tx, 'Ty':ty, 'Tz':tz}, 
-                'Orientation':{'Ox':ox, 'Oy':oy, 'Oz':oz, 'Ow':ow} }
-
-    return pose_dict
 
 
 def main(filepath, start, end, dir_path):
@@ -159,7 +132,7 @@ def main(filepath, start, end, dir_path):
             if svo_position >= (nb_frames * end / 100):
                 break
             
-            print(f"processing {i}th frame out of {num_frames}")
+            #print(f"processing {i}th frame out of {num_frames}")
             output_dir = os.path.join(dir_path, "frame_{}/images".format(i) )
             pc_dir = os.path.join(dir_path, "frame_{}/pointcloud".format(i) )
             pose_dir = os.path.join(dir_path, "frame_{}/pose".format(i) )
@@ -169,7 +142,7 @@ def main(filepath, start, end, dir_path):
 
 
             # A new image is available if grab() returns SUCCESS
-            print("Writing images")
+            #print("Writing images")
             zed.retrieve_image(image, sl.VIEW.LEFT)
             zed.retrieve_image(image_r, sl.VIEW.RIGHT)
             image.write( os.path.join(output_dir, 'left_image.jpg') )
