66c6a166eff6925c6ae1658dcbd05352638bbc41
Shubham Kumar <skumar@monarchtractor.com>
2024-07-21 03:23:48 +0530

    [main.py] --> integrated RBA

--- main.sh
+++ main.sh
@@ -67,11 +67,11 @@ echo "==============================="
 echo -e "\n"
 
 
-python3 "${PIPELINE_SCRIPT_DIR}/svo_to_pointcloud.py" \
-	--svo_path=$SVO_FILE_PATH\
-	--start_frame=$SVO_START_IDX\
-	--end_frame=$SVO_END_IDX\
-	--output_dir=$SVO_IMAGES_DIR
+# python3 "${PIPELINE_SCRIPT_DIR}/svo_to_pointcloud.py" \
+# 	--svo_path=$SVO_FILE_PATH\
+# 	--start_frame=$SVO_START_IDX\
+# 	--end_frame=$SVO_END_IDX\
+# 	--output_dir=$SVO_IMAGES_DIR
 
 
 # [SVO STEREO IMAGES ==> SPARSE RECONSTRUCTION]
@@ -87,54 +87,53 @@ echo "SPARSE_RECON_OUTPUT_DIR: $SPARSE_RECON_OUTPUT_DIR"
 echo "==============================="
 echo -e "\n"
 
-python3 "${PIPELINE_SCRIPT_DIR}/sparse-reconstruction.py" \
-    --svo_images=$SVO_IMAGES_DIR \
-	--input_dir=$SPARSE_RECON_INPUT_DIR \
-	--output_dir=$SPARSE_RECON_OUTPUT_DIR \
-	--svo_file=$SVO_FILE_PATH  
+# python3 "${PIPELINE_SCRIPT_DIR}/sparse-reconstruction.py" \
+#     --svo_images=$SVO_IMAGES_DIR \
+# 	--input_dir=$SPARSE_RECON_INPUT_DIR \
+# 	--output_dir=$SPARSE_RECON_OUTPUT_DIR \
+# 	--svo_file=$SVO_FILE_PATH  
 
 
 # [RIG BUNDLE ADJUSTMENT]
-RBA_INPUT_DIR="${SPARSE_RECONSTRUCTION_LOC}/output/ref_locked/"
-RBA_OUTPUT_DIR="${SPARSE_RECONSTRUCTION_LOC}/output/rba/"
-RIG_CONFIG_PATH="config/rig.json"
+RBA_INPUT_DIR="${SPARSE_RECON_OUTPUT_DIR}/ref_locked/"
+RBA_OUTPUT_DIR="${PIPELINE_OUTPUT_DIR}/rig-bundle-adjustment/${SVO_FILENAME_WITH_IDX}"
+RBA_CONFIG_PATH="${PIPELINE_CONFIG_DIR}/rig.json"
 
 echo -e "\n"
 echo "==============================="
 echo "[RIG BUNDLE ADJUSTMENT]"
 echo "RBA_INPUT_DIR: $RBA_INPUT_DIR"
 echo "RBA_OUTPUT_DIR: $RBA_OUTPUT_DIR"
-echo "==============================="s
+echo "RBA_CONFIG_PATH: $RBA_CONFIG_PATH"
+echo "==============================="
 echo -e "\n"
+
+rm -rf "${RBA_OUTPUT_DIR}"
+mkdir -p "${RBA_OUTPUT_DIR}"
+
+$COLMAP_EXE_PATH/colmap rig_bundle_adjuster \
+	--input_path $RBA_INPUT_DIR \
+	--output_path $RBA_OUTPUT_DIR \
+	--rig_config_path $RBA_CONFIG_PATH \
+	--BundleAdjustment.refine_focal_length 0 \
+	--BundleAdjustment.refine_principal_point 0 \
+	--BundleAdjustment.refine_extra_params 0 \
+	--BundleAdjustment.refine_extrinsics 1 \
+	--BundleAdjustment.max_num_iterations 500 \
+	--estimate_rig_relative_poses False
+
 
 exit 1
 
-# RIG_OUTPUT_PATH="${RIG_BUNDLE_ADJUSTER_LOC}/output/"
-
-# rm -rf $RIG_OUTPUT_PATH
-# mkdir -p "$RIG_OUTPUT_PATH"
-
-# $COLMAP_EXE_PATH/colmap rig_bundle_adjuster \
-# 	--input_path $RIG_INPUT_PATH \
-# 	--output_path $RIG_OUTPUT_PATH \
-# 	--rig_config_path $RIG_CONFIG_PATH \
-# 	--BundleAdjustment.refine_focal_length 0 \
-# 	--BundleAdjustment.refine_principal_point 0 \
-# 	--BundleAdjustment.refine_extra_params 0 \
-# 	--BundleAdjustment.refine_extrinsics 1 \
-# 	--BundleAdjustment.max_num_iterations 500 \
-# 	--estimate_rig_relative_poses False
-
-
 # ====== DENSE RECONSTRUCTION =======================
 
-rm -rf $dense_sfm_path
-DENSE_RECONSTRUCTION_LOC="../dense-reconstruction"
-#srun --gres=gpu:1 \
-	python "$(pwd)/${DENSE_RECONSTRUCTION_LOC}/scripts/dense-reconstruction.py" \
-    	--mvs_path="$dense_sfm_path" \
-    	--output_path="$RIG_OUTPUT_PATH" \
-	--image_dir="$SPARSE_DATA_LOC" \
+# rm -rf $dense_sfm_path
+# DENSE_RECONSTRUCTION_LOC="../dense-reconstruction"
+# #srun --gres=gpu:1 \
+# 	python "$(pwd)/${DENSE_RECONSTRUCTION_LOC}/scripts/dense-reconstruction.py" \
+#     	--mvs_path="$dense_sfm_path" \
+#     	--output_path="$RIG_OUTPUT_PATH" \
+# 	--image_dir="$SPARSE_DATA_LOC" \
 
 
-echo "****************************** DONE ************************"
+# echo "****************************** DONE ************************"
