aec78e5d8003e7193bf31b754ee516a9e17bc892
Shubham Kumar <skumar@monarchtractor.com>
2024-07-21 03:05:48 +0530

    [main.sh] --> cleanup

--- main.sh
+++ main.sh
@@ -37,14 +37,21 @@ echo "SVO_END_IDX: $SVO_END_IDX"
 echo "==============================="
 echo -e "\n"
 
-INPUT_DIR="input" 
-OUTPUT_DIR="output"
+
+# [PIPELINE PARAMS]
 SVO_FILENAME_WITH_IDX="${SVO_FILENAME}_${SVO_START_IDX}_${SVO_END_IDX}"
-PIPELINE_SCRIPTS_DIR="scripts"
+COLMAP_EXE_PATH=/usr/local/bin
+
+PIPELINE_SCRIPT_DIR="scripts"
+PIPELINE_CONFIG_DIR="config"
+PIPELINE_INPUT_DIR="input" 
+PIPELINE_OUTPUT_DIR="output"
+
+
 
 # [SVO FILE ==> STEREO IMAGES]
-SVO_INPUT_DIR="${INPUT_DIR}/svo-files"
-SVO_OUTPUT_DIR="${OUTPUT_DIR}/stereo-images"
+SVO_INPUT_DIR="${PIPELINE_INPUT_DIR}/svo-files"
+SVO_OUTPUT_DIR="${PIPELINE_OUTPUT_DIR}/stereo-images"
 
 SVO_FILE_PATH="${SVO_INPUT_DIR}/${SVO_FILENAME}"
 SVO_IMAGES_DIR="${SVO_OUTPUT_DIR}/${SVO_FILENAME_WITH_IDX}"
@@ -60,7 +67,7 @@ echo "==============================="
 echo -e "\n"
 
 
-python3 "${PIPELINE_SCRIPTS_DIR}/svo_to_pointcloud.py" \
+python3 "${PIPELINE_SCRIPT_DIR}/svo_to_pointcloud.py" \
 	--svo_path=$SVO_FILE_PATH\
 	--start_frame=$SVO_START_IDX\
 	--end_frame=$SVO_END_IDX\
@@ -68,41 +75,55 @@ python3 "${PIPELINE_SCRIPTS_DIR}/svo_to_pointcloud.py" \
 
 
 # [SVO STEREO IMAGES ==> SPARSE RECONSTRUCTION]
-SPARSE_RECON_INPUT_DIR="${INPUT_DIR}/sparse-reconstruction/${SVO_FILENAME_WITH_IDX}"
-SPARSE_RECON_OUTPUT_DIR="${OUTPUT_DIR}/sparse-reconstruction/${SVO_FILENAME_WITH_IDX}"
+SPARSE_RECON_INPUT_DIR="${PIPELINE_INPUT_DIR}/sparse-reconstruction/${SVO_FILENAME_WITH_IDX}"
+SPARSE_RECON_OUTPUT_DIR="${PIPELINE_OUTPUT_DIR}/sparse-reconstruction/${SVO_FILENAME_WITH_IDX}"
 
-python3 "$(pwd)/${SPARSE_RECONSTRUCTION_LOC}/scripts/sparse-reconstruction.py" \
+
+echo -e "\n"
+echo "==============================="
+echo "[SVO STEREO IMAGES --> SPARSE RECONSTRUCTION]"
+echo "SPARSE_RECON_INPUT_DIR: $SPARSE_RECON_INPUT_DIR"
+echo "SPARSE_RECON_OUTPUT_DIR: $SPARSE_RECON_OUTPUT_DIR"
+echo "==============================="
+echo -e "\n"
+
+python3 "${PIPELINE_SCRIPT_DIR}/sparse-reconstruction.py" \
     --svo_images=$SVO_IMAGES_DIR \
 	--input_dir=$SPARSE_RECON_INPUT_DIR \
 	--output_dir=$SPARSE_RECON_OUTPUT_DIR \
 	--svo_file=$SVO_FILE_PATH  
 
 
-# ========= RIG BUNDLE ADJUSTMENT =====================
-
-COLMAP_EXE_PATH=/usr/local/bin
-RIG_BUNDLE_ADJUSTER_LOC="../rig-bundle-adjuster"
-RIG_INPUT_PATH="${SPARSE_RECONSTRUCTION_LOC}/output/ref_locked/"
-#RIG_INPUT_PATH=$(realpath "$RIG_INPUT_PATH")
-
-echo "RIG_INPUT_PATH: $RIG_INPUT_PATH"
-
-RIG_OUTPUT_PATH="${RIG_BUNDLE_ADJUSTER_LOC}/output/"
+# [RIG BUNDLE ADJUSTMENT]
+RBA_INPUT_DIR="${SPARSE_RECONSTRUCTION_LOC}/output/ref_locked/"
+RBA_OUTPUT_DIR="${SPARSE_RECONSTRUCTION_LOC}/output/rba/"
 RIG_CONFIG_PATH="config/rig.json"
 
-rm -rf $RIG_OUTPUT_PATH
-mkdir -p "$RIG_OUTPUT_PATH"
+echo -e "\n"
+echo "==============================="
+echo "[RIG BUNDLE ADJUSTMENT]"
+echo "RBA_INPUT_DIR: $RBA_INPUT_DIR"
+echo "RBA_OUTPUT_DIR: $RBA_OUTPUT_DIR"
+echo "==============================="s
+echo -e "\n"
 
-$COLMAP_EXE_PATH/colmap rig_bundle_adjuster \
-	--input_path $RIG_INPUT_PATH \
-	--output_path $RIG_OUTPUT_PATH \
-	--rig_config_path $RIG_CONFIG_PATH \
-	--BundleAdjustment.refine_focal_length 0 \
-	--BundleAdjustment.refine_principal_point 0 \
-	--BundleAdjustment.refine_extra_params 0 \
-	--BundleAdjustment.refine_extrinsics 1 \
-	--BundleAdjustment.max_num_iterations 500 \
-	--estimate_rig_relative_poses False
+exit 1
+
+# RIG_OUTPUT_PATH="${RIG_BUNDLE_ADJUSTER_LOC}/output/"
+
+# rm -rf $RIG_OUTPUT_PATH
+# mkdir -p "$RIG_OUTPUT_PATH"
+
+# $COLMAP_EXE_PATH/colmap rig_bundle_adjuster \
+# 	--input_path $RIG_INPUT_PATH \
+# 	--output_path $RIG_OUTPUT_PATH \
+# 	--rig_config_path $RIG_CONFIG_PATH \
+# 	--BundleAdjustment.refine_focal_length 0 \
+# 	--BundleAdjustment.refine_principal_point 0 \
+# 	--BundleAdjustment.refine_extra_params 0 \
+# 	--BundleAdjustment.refine_extrinsics 1 \
+# 	--BundleAdjustment.max_num_iterations 500 \
+# 	--estimate_rig_relative_poses False
 
 
 # ====== DENSE RECONSTRUCTION =======================
