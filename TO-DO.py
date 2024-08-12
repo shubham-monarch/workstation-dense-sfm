e6b9643484be67896361ea17ce75590a97b4868e
Shubham Kumar <skumar@monarchtractor.com>
2024-08-12 14:13:35 +0530

    updated TO-DO

---
+++ TO-DO.py
@@ -0,0 +1,57 @@
+# - aws api integration
+# - aws / local input detection
+
+# - output-backend -> output folder
+# - aws instance setup
+# 	- add colmap cmake update to s pycolmap installtion 
+# 	- retag colmap , pycolmap
+# 	- update requirements.txt
+# 	- update pipeline tag
+# - processed/unprocessed folders
+# 	- add svo files
+# 	- cleanup before running the pipelines
+# - documentation
+# 	- update release notes
+# 	- update installation steps
+# 	- add + update setup.mds
+# 	- add readme.md
+# 	- update colmap installation steps in setup.md
+# - add segmentation inference script
+
+# - check if script is being executed from the project root
+# - dense reconstruction support for multiple gpus
+# - [error-handling / folder deletion] for Ctrl-C / unexpected script termination 
+# - update default bb params for pointcloud cropping
+# - return -1 for failed jsons
+# - add colmap cmake update to support pycolmap installtion 
+# - retag colmap , pycolmap
+# - update COLMAP tags with pycolmap bindings integration
+# - update requirements.txt
+# - update pipeline tag
+# - cleanup for all the modules
+# - aws integration 
+# - add main-ws.sh, main-aws.sh
+# - aws integration
+# - move all files inside input to a folder
+# - add status-processed / unprocessed folders for user feeback
+# - move output-backend ---> output script
+# - output/input-backend clean-up
+# - add images  support
+# - check if script is being executed from the project root
+# - dense reconstruction support for multiple gpus
+# - [error-handling / folder deletion] for Ctrl-C / unexpected script termination 
+# - update default bb params for pointcloud cropping
+# - user feedback mechanism 
+# - module post-processing cleaning => [vo / dense ]
+# - fix script kill issue
+# - modularize python script calls in main-file.sh 
+# - change output-dir name for p360 module
+# - tracking weight files
+# - check with p360 module works with updated PLY fsiles
+# - fix number of points in different in ply-rgb and ply-seg
+# - add labels to colmap points3d 
+# - python script existing folder detection
+# - automate uploading to aws
+
+
+
